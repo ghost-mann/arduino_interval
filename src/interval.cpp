6cf8a06372f1bca50078e83adc0f7ff38c0ed5e5
ghost-mann <iamaob8@protonmail.com>
2026-01-12 15:31:49 +0300

    feat:added  template code in interval.cpp

---
+++ src/interval.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include "interval.h"
+
+const int ledPin = 9;
+
+unsigned long previousMills = 0;
+
+const long interval = 1000;
+
+void init_interval() {
+
+
+}
+
+void init_loop() {
+    
+}
